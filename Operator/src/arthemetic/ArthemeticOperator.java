cc801a330943f1fc0dc1762c466671722b5a621e
sunithareddy54 <sunithareddy554@gmail.com>
2018-09-30 11:44:39 -0400

    Initial commit

---
+++ Operator/src/arthemetic/ArthemeticOperator.java
@@ -0,0 +1,11 @@
+package arthemetic;
+
+public class ArthemeticOperator {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+        int c = a+b ;
+
+        System.out.println(c * c);
+    }
+}
